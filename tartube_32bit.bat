5c5ac43ca04c92749e29c9800799f77eadf00806
candrapersada <36833664+candrapersada@users.noreply.github.com>
2020-12-21 06:07:11 +0700

    Update tartube_32bit.bat

--- tartube_32bit.bat
+++ tartube_32bit.bat
@@ -1,7 +1,7 @@
 @echo off
 ..\..\..\usr\bin\mintty.exe -w hide /bin/env MSYSTEM=MINGW32 /bin/bash -lc /home/user/tartube/tartube_mswin.sh
 echo ============================
-echo welcome to Tartube
+echo welcome to Tartube 
 echo ============================
 timeout 5 > NUL
 echo %time%
