7b1288963979d8632830b0b200eb951653276cae
candrapersada <36833664+candrapersada@users.noreply.github.com>
2020-12-21 11:34:19 +0700

    Update tartube_64bit.bat

--- tartube_64bit.bat
+++ tartube_64bit.bat
@@ -1,5 +1,6 @@
 @echo off
 ..\..\..\usr\bin\mintty.exe -w hide /bin/env MSYSTEM=MINGW64 /bin/bash -lc /home/user/tartube/tartube_mswin.sh
+title Tartube
 echo ============================
 echo welcome to Tartube
 echo ============================
