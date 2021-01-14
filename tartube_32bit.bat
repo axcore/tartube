4f37c3dad3afd9ce67704ec74c995280da96147d
candrapersada <36833664+candrapersada@users.noreply.github.com>
2021-01-14 10:39:44 +0700

    Add files via upload
    Hide Command Window

---
+++ tartube_32bit.bat
@@ -0,0 +1,2 @@
+@echo off & title Tartube
+..\..\..\usr\bin\mintty.exe -w hide /bin/env MSYSTEM=MINGW32 /bin/bash -lc /home/user/tartube/tartube_mswin.sh
---
+++ tartube_64bit.bat
@@ -0,0 +1,2 @@
+@echo off & title Tartube
+..\..\..\usr\bin\mintty.exe -w hide /bin/env MSYSTEM=MINGW64 /bin/bash -lc /home/user/tartube/tartube_mswin.sh
