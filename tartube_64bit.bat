c585f9c2dd8d7a1968f73092195983cc86e7ad72
candrapersada <36833664+candrapersada@users.noreply.github.com>
2021-01-14 10:19:40 +0700

    Delete tartube_64bit.bat

--- tartube_64bit.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-..\..\..\usr\bin\mintty.exe -w hide /bin/env MSYSTEM=MINGW64 /bin/bash -lc /home/user/tartube/tartube_mswin.sh
-title Tartube
-echo ============================
-echo welcome to Tartube
-echo ============================
-timeout 5 > NUL
-echo %time%
