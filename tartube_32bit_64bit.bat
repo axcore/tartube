6b6775b1df4f913314ac75461d2ecf87a508e153
candrapersada <36833664+candrapersada@users.noreply.github.com>
2021-01-14 10:28:45 +0700

    Update tartube_32bit_64bit.bat

--- tartube_32bit_64bit.bat
+++ tartube_32bit_64bit.bat
@@ -1,12 +1,3 @@
-@echo off
+@echo off & title Tartube
 start ..\..\..\usr\bin\mintty.exe -w hide /bin/env MSYSTEM=MINGW32 /bin/bash -lc /home/user/tartube/tartube_mswin.sh
 start ..\..\..\usr\bin\mintty.exe -w hide /bin/env MSYSTEM=MINGW64 /bin/bash -lc /home/user/tartube/tartube_mswin.sh
-title Tartube
-echo ============================
-echo welcome to Tartube
-echo ============================
-timeout 4 > nul
-echo ============================
-echo Tartube Start
-echo ============================
-timeout 3 > nul
