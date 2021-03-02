ed49d0d4275000467c5765c43986eeeccaea9839
A S Lewis <aslewis@cpan.org>
2021-03-02 09:31:43 +0000

    Fix no start on MSWin if Python2 installed

--- tartube_32bit.bat
+++ tartube_32bit.bat
@@ -1,1 +1,2 @@
+set PYTHONHOME=
 ..\..\..\usr\bin\mintty.exe -w hide /bin/env MSYSTEM=MINGW32 /bin/bash -lc /home/user/tartube/tartube_mswin.sh
--- tartube_64bit.bat
+++ tartube_64bit.bat
@@ -1,1 +1,2 @@
+set PYTHONHOME=
 ..\..\..\usr\bin\mintty.exe -w hide /bin/env MSYSTEM=MINGW64 /bin/bash -lc /home/user/tartube/tartube_mswin.sh
