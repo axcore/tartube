a036ff39d2ede8daf985799c1df74c64d16de4e8
A S Lewis <aslewis@cpan.org>
2022-05-12 12:18:30 +0100

    Update to v2.4.042

--- setup.py
+++ setup.py
@@ -190,7 +190,7 @@ for path in glob.glob('sounds/*'):
 # Setup
 setuptools.setup(
     name='tartube',
-    version='2.4.038',
+    version='2.4.042',
     description='GUI front-end for youtube-dl',
     long_description=long_description,
     long_description_content_type='text/plain',
